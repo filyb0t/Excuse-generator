098aaf1de8dfde357343db922f96f920847d6df5
Felix Uriel Lara Castro <138715170+filyb0t@users.noreply.github.com>
2023-09-11 20:10:36 +0000

    generador

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
   document;
-  console.log("Hello Rigo from the console!");
+  console.log("Hello im rigo from the console!");
 };
